5b21184c65cb9c0b32636aedd85279c8efd1edf2
Avinash Kumar <61050429+raj572@users.noreply.github.com>
2023-08-09 17:58:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ var typed = new Typed(".auto-input", {
     backSpeed:100,
     loop:true
 })
+
+// skill bar
+document.addEventListener("DOMContentLoaded", function () {
+    const skills = [
+      { label: "HTML", progress: 90 },
+      { label: "CSS", progress: 80 },
+      { label: "JavaScript", progress: 30 },
+      { label: "Python", progress: 90 },
+      { label: "DSA", progress: 55 },
+      { label: "MS office", progress: 65 },
+      { label: "C++", progress: 85 },
+    ];
+
+    skills.forEach((skill, index) => {
+      const progressBar = new ProgressBar.Line(`#progress${index + 1}`, {
+        color: "aqua",
+        trailColor: "#f0f0f0",
+        strokeWidth: 10,
+        duration: 2000,
+        easing: "easeInOut",
+      });
+
+      progressBar.animate(skill.progress / 100);
+    });
+  });
